b4c9f401803d703521274a69ec6affbd45839561
Emanuel <emanuelvitorino.emagine@milleniumbcp.pt>
2025-09-10 16:55:58 +0100

    moved radius environment to clusters dir

--- environments/radius/main.tf
+++ environments/radius/main.tf
@@ -11,5 +11,5 @@ module "flux" {
   source = "../../modules/flux"
   ssh_key = "/home/emanuel/.ssh/id_rsa"
   kubeconfig = "~/.kube/config"
-  bootstrapDir = "dev/radius"
+  bootstrapDir = "clusters/radius"
 }
